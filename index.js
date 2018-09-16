4428f2a3feb7bc58a2eeea6268573938ba463f60
112madgamer <ryleegeorge1@gmail.com>
2018-09-16 19:26:16 +1000

    Cleanup and streamline stuff aswell as adding a config file

--- index.js
+++ index.js
@@ -1,40 +1,70 @@
 const fs = require('fs')
 const winston = require('winston')
+const path = require('path')
+const ComicVine = require('../ComicVineWrapper')
+const config = require('./config.json')
+const fsr = require('fs-readdir-recursive')
+let client = new ComicVine(config.API_TOKEN)
+
+const logger = winston.createLogger({
+  transports: [new winston.transports.Console()],
+  format: winston.format.combine(
+    winston.format.colorize({ all: true }),
+    winston.format.simple()
+  )
+})
+
 async function run () {
-  const files = await fs.readdirSync('./unsorted/')
+  let comics = await fsr('./unsorted')
+  if (!fs.existsSync('./unsorted/')) {
+    fs.mkdirSync('./unsorted/')
+  }
   // const stripRegex = /\(.*?\)/gim
   // const issueRegex = / \d([^)a-z]+)\d /gi
-  const logger = winston.createLogger({
-    transports: [new winston.transports.Console()],
-    format: winston.format.combine(
-      winston.format.colorize({ all: true }),
-      winston.format.simple()
-    )
-  })
 
+  ComicSort(comics)
+}
+
+async function ComicSort (comics) {
   const yearRegex = /\(([^)a-zA-Z]+)\)/gm
   const seriesRegex = /^[^\\(]+/gm
   const issue2Regex = /\d+\b/gim
+  let publisher
 
-  for (let comic of files) {
-    if (comic.endsWith('.cbz') || comic.endsWith('.cbr')) {
-      let comicYear = comic.match(yearRegex)
-      let comicName = comic.replace(issue2Regex, '').match(seriesRegex)
+  for (let comic of comics) {
+    let filename = path.basename(comic)
+    if (filename.endsWith('.cbz') || filename.endsWith('.cbr')) {
+      let comicYear = filename.match(yearRegex)
+      let comicName = filename.replace(issue2Regex, '').match(seriesRegex).toString().trim()
+      let comicIssue = filename.replace(yearRegex, '').match(issue2Regex)
 
-      if (!fs.existsSync(`./sorted/${comicName.toString().trim()} ${comicYear}`)) {
-        logger.log('info', `Creating dir: ./sorted/${comicName.toString().trim()} ${comicYear}`)
-        fs.mkdirSync(`./sorted/${comicName.toString().trim()} ${comicYear}`)
+      logger.log('info', `Querying ComicVine for "${comicName}"`)
+
+      let query = await client.search.searchComic({
+        query: `${comicName}`,
+        sort: 'name:desc'
+      })
+      let _comic = query.results[0]
+      if (typeof _comic === 'undefined') {
+        publisher = 'Unknown'
       } else {
-        logger.warn(`./sorted/${comicName.toString().trim()} ${comicYear} ALREADY EXISTS!`)
+        publisher = _comic.publisher.name
       }
-      if (!fs.existsSync(`./sorted/${comicName.toString().trim()} ${comicYear}/${comic}`)) {
-        // logger.log('info', `Copying file: ${comic} ==> ./sorted/${comicName.toString().trim()} ${comicYear}/`)
-        fs.copyFileSync(`./unsorted/${comic}`, `./sorted/${comicName.toString().trim()} ${comicYear}/${comic}`, (err) => {
+
+      if (!fs.existsSync(`./sorted/${publisher}/`)) {
+        fs.mkdirSync(`./sorted/${publisher}/`)
+      }
+      if (!fs.existsSync(`./sorted/${publisher}/${comicName} ${comicYear}`)) {
+        logger.log('info', `Creating dir: ./sorted/${publisher}/${comicName} ${comicYear}`)
+        fs.mkdirSync(`./sorted/${publisher}/${comicName} ${comicYear}`)
+      }
+      if (!fs.existsSync(`./sorted/${publisher}/${comicName} ${comicYear}/${filename}`)) {
+        fs.copyFileSync(comic, `./sorted/${publisher}/${comicName} ${comicYear}/${filename}`, (err) => {
           if (err) throw err
-          logger.log('info', `Copied file: ${comic} ==> ./sorted/${comicName.toString().trim()} ${comicYear}/`)
+          logger.log('info', `Copied file: ${comic} ==> ./sorted/${publisher}/${comicName} ${comicYear}/`)
         })
       } else {
-        logger.warn(`./unsorted/${comicName.toString().trim()} ${comicYear}/${comic} ALREADY EXISTS!`)
+        logger.warn(`./sorted/${publisher}/${comicName} ${comicYear}/${filename} ALREADY EXISTS!`)
       }
     }
   }
